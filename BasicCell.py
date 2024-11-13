e04cace08354b7cd4c05702c8a77bc41488844a5
liav2002 <liavmail00@gmail.com>
2024-11-13 21:00:46 +0200

    Implement BasicCell Class

--- BasicCell.py
+++ BasicCell.py
@@ -1,8 +1,14 @@
-import Cell
+from Cell import Cell
 
 class BasicCell(Cell):
     def __init__(self, is_alive):
-        super(is_alive)
+        super().__init__(is_alive)
 
     def determine_next_state(self, neighbors):
-        pass
+        alive_neighbors = sum([1 for neighbor in neighbors if neighbor.is_alive()])
+
+        if self.is_alive() and (alive_neighbors < 2 or alive_neighbors > 3):
+                self.toggle_state() # change to die
+
+        elif alive_neighbors == 3:
+            self.toggle_state() # change to alive
