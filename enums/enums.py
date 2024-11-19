ecedeb4bf74cc6ad94792de7d93eb8cab9bb58cf
liav2002 <liavmail00@gmail.com>
2024-11-19 15:24:18 +0200

    Fix the issue at herbibore reproduce count. BUG: num_of_predators_died is wrong.

--- enums/enums.py
+++ enums/enums.py
@@ -2,5 +2,5 @@ CELL_CHARACTER = {"basic_cell": "0", "empty_cell": 128307, "plant_cell": 127793,
                   "tree_cell": "T"}
 CELL_TYPE = {"Empty": "empty_cell", "Basic": "basic_cell", "Plant": "plant_cell", "Herbivore": "herbivore_cell",
              "Predator": "predator_cell", "Rock": "rock_cell", "Tree": "tree_cell"}
-EVENT = {"PLANT_DIED": '0', "HERBIVORE_DIED": '1', "PREDATOR_DIED": '2', "HERBIVORE_REPRODUCE": '3', "MOVE": '3',
-         "NEW_GENERATION": '4', 'CELL_REVIVAL': '5', 'PLANT_WAS_EATEN': '6', 'HERBIVORE_WAS_EATEN': '7'}
+EVENT = {"PLANT_DIED": '0', "HERBIVORE_DIED": '1', "PREDATOR_DIED": '2', "HERBIVORE_REPRODUCE": '3', "MOVE": '4',
+         "NEW_GENERATION": '5', 'CELL_REVIVAL': '6', 'PLANT_WAS_EATEN': '7', 'HERBIVORE_WAS_EATEN': '8'}
